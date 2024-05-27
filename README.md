30b74067445467fcdcd0198b086f034ebc3ca073
Jan Rádl <radl.honza@gmail.com>
2024-05-27 08:16:36 +0200

    Favicon

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
 ## Nový web Smyčcem.cz
 
 # Používá pokusně pnpm
+
 - Install pnpm `brew install pnpm`
 - Install packages `pnpm i`
 - Add package `pnpm add the-package`
 
 # Todo:
-- [ ] Youtube video na HP
+
 - [ ] youtube /carousel/ na "Hudba"
 - [ ] Stranka kontakty
 - [ ] Metadata
-- [ ] Favicona
-
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import "./global.css";
-import type { Metadata } from "next";
-import { ViewTransitions } from "next-view-transitions";
-import { baseUrl } from "./basepath";
-import classNames from "classnames";
-import { GeistSans } from "geist/font/sans";
-import Navbar from "./components/nav";
-import Background from "./components/background";
+import './global.css';
+import type { Metadata } from 'next';
+import { ViewTransitions } from 'next-view-transitions';
+import { baseUrl } from './basepath';
+import classNames from 'classnames';
+import { GeistSans } from 'geist/font/sans';
+import Navbar from './components/nav';
+import Background from './components/background';
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
-    default: "Trhni si smyčcem",
-    template: "%s – Trhni si smyčcem",
+    default: 'Trhni si smyčcem',
+    template: '%s – Trhni si smyčcem',
   },
   description:
-    "Trhni si smyčcem je komorní těleso mezi punkem a filharmonií. Hrajeme vlastní písně. Je nás 6 a máme klasické hudební nástroje. Nejlépe se nám vystupuje mezi skvělými lidmi v zajímavých prostorech a v přírodě. Naše písně reflektují sociální, filosofická a zahradnická témata. Patříme pod magický realismus.",
+    'Trhni si smyčcem je komorní těleso mezi punkem a filharmonií. Hrajeme vlastní písně. Je nás 6 a máme klasické hudební nástroje. Nejlépe se nám vystupuje mezi skvělými lidmi v zajímavých prostorech a v přírodě. Naše písně reflektují sociální, filosofická a zahradnická témata. Patříme pod magický realismus.',
   openGraph: {
-    title: "Trhni si smyčcemo",
+    title: 'Trhni si smyčcemo',
     description:
-      "Trhni si smyčcem je komorní těleso mezi punkem a filharmonií. Hrajeme vlastní písně. Je nás 6 a máme klasické hudební nástroje. Nejlépe se nám vystupuje mezi skvělými lidmi v zajímavých prostorech a v přírodě. Naše písně reflektují sociální, filosofická a zahradnická témata. Patříme pod magický realismus.",
+      'Trhni si smyčcem je komorní těleso mezi punkem a filharmonií. Hrajeme vlastní písně. Je nás 6 a máme klasické hudební nástroje. Nejlépe se nám vystupuje mezi skvělými lidmi v zajímavých prostorech a v přírodě. Naše písně reflektují sociální, filosofická a zahradnická témata. Patříme pod magický realismus.',
     url: baseUrl,
-    siteName: "Trhni si smyčcem",
-    locale: "cs_CZ",
-    type: "website",
+    siteName: 'Trhni si smyčcem',
+    locale: 'cs_CZ',
+    type: 'website',
   },
   robots: {
     index: true,
@@ -30,9 +30,9 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
     },
   },
 };
@@ -45,10 +45,40 @@ export default function RootLayout({
   return (
     <ViewTransitions>
       <html lang="cs" className="h-full bg-black">
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#000000"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
         <body
           className={classNames(
             GeistSans.variable,
-            "h-full overflow-hidden print:overflow-visible"
+            'h-full overflow-hidden print:overflow-visible'
           )}
         >
           <Background />
