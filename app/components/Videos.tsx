664915974f01c09881bdfc27349ccc3b77ed09c3
Tomas Randus <tom.randus@gmail.com>
2025-02-16 20:46:00 +0100

    video grid

--- app/components/Videos.tsx
+++ app/components/Videos.tsx
@@ -48,34 +48,31 @@ const Lines = ({
   expanded: boolean;
 }) => (
   <>
-    {lines.map((line, i) => (
-      <ul
-        className={classNames(
-          className,
-          "overflow-hidden transition-all",
-          i > 0 && !expanded ? "h-0" : ""
-        )}
-        key={line.map(({ slug }) => slug).join()}
-      >
-        {line.map((song) =>
-          "metadata" in song ? (
-            <Video key={song.slug} {...song} />
-          ) : (
-            <a
-              key={song.slug}
-              href="https://www.youtube.com/@smyccem"
-              target="_blank"
-              className="group block relative border-white border-3 cursor-pointer aspect-video animation-magic-backdrop text-center"
-            >
-              <div className="mt-2">Náš Youtube kanál</div>
-              <PiYoutubeLogo className="absolute inset-[50%] translate-[-50%] text-4xl lg:text-6xl group-hover:text-3xl group-hover:lg:text-5xl transition-all" />
-
-              {/* <PiYoutubeLogo className="w-8 h-8 md:w-10 md:h-10 ml-1" /> */}
-            </a>
-          )
-        )}
-      </ul>
-    ))}
+    {lines.map((line, i) => {
+      if (i > 0 && !expanded) return null;
+      return (
+        <ul
+          className={classNames(className, "overflow-hidden transition-all")}
+          key={line.map(({ slug }) => slug).join()}
+        >
+          {line.map((song) =>
+            "metadata" in song ? (
+              <Video key={song.slug} {...song} />
+            ) : (
+              <a
+                key={song.slug}
+                href="https://www.youtube.com/@smyccem"
+                target="_blank"
+                className="group relative border-white border-3 cursor-pointer aspect-video animation-magic-backdrop grid grid-rows-3 place-items-center"
+              >
+                <div className="justify-center">Náš Youtube kanál</div>
+                <PiYoutubeLogo className=" text-4xl lg:text-5xl group-hover:text-5xl group-hover:lg:text-6xl transition-all" />
+              </a>
+            )
+          )}
+        </ul>
+      );
+    })}
   </>
 );
 
@@ -86,15 +83,20 @@ export default function Videos({ songs }: { songs: Song[] }) {
     "text-lg sm:text-xl [&>svg]:text-2xl sm:[&>svg]:text-3xl text-white flex gap-1 items-center cursor-pointer whitespace-nowrap relative hover:top-[2px] transition-all ";
 
   return (
-    <div className="pb-4">
+    <div className="pb-4 space-y-1 md:space-y-2">
       <Lines
         lines={toLines(songs, 3)}
-        className="hidden md:block space-y-1 lg:space-y-2 columns-3"
+        className="hidden sm:block columns-3"
         expanded={expanded}
       />
       <Lines
         lines={toLines(songs, 2)}
-        className="md:hidden space-y-1 lg:space-y-2 columns-2"
+        className="hidden xs:max-sm:block sm:hidden columns-2"
+        expanded={expanded}
+      />
+      <Lines
+        lines={toLines(songs, 1)}
+        className="xs:hidden"
         expanded={expanded}
       />
       <div className="flex justify-center">
